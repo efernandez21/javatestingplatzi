942314698e7b562e90ceda8b11d7fcb907b2ae3e
elkinfernandez21 <elkinfernandez21@gmail.com>
2021-12-15 09:02:42 -0500

    Implementacion de Test con JUnit para la clase StringUtil con test correcto

--- src/test/java/com/platzi/javatestingplatzi/util/StringUtilTest.java
+++ src/test/java/com/platzi/javatestingplatzi/util/StringUtilTest.java
@@ -1,27 +1,23 @@
 package com.platzi.javatestingplatzi.util;
 
 
-//import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Clase para testeado de lo ya contruido, tenemos un main para la prueba del metodo aqui en la clase de test
  */
 public class StringUtilTest {
-    public static void main(String[] args) {
+
+    @Test
+    public  void testRepeat() {
+
         //Llamado a la funcion a probar
-        //Usando el metodo creado sin el de Junit
-        assertEquals(StringUtil.repeat("hola",3),"holaholahola");
-        assertEquals(StringUtil.repeat("hola",1),"hola");
+        //Usando la funcion assertEquals de Junit
+        Assert.assertEquals("holaholahola",StringUtil.repeat("hola",3));
+        Assert.assertEquals("hola", StringUtil.repeat("hola",1));
     }
 
-    /**
-     * Autodefinicion del metodo assertEquals sin hacer uso del JUnit
-     * @param actual valor actual
-     * @param expected valor esperado o el correcto
-     */
-    private static void assertEquals(String actual, String expected) {
-        //Comprobacion del resultado con el if
-        if (!actual.equals(expected)){
-            throw new RuntimeException(actual +" is not Equal than "+expected);
-        }
-    }
+
 }
