6b28fd644b38fd94df0b77405457ff3bc0714c51
elkinfernandez21 <elkinfernandez21@gmail.com>
2021-12-17 18:50:39 -0500

    Reto 1: Funcion Empty y respectivos Tests

--- src/main/java/com/platzi/javatestingplatzi/util/StringUtil.java
+++ src/main/java/com/platzi/javatestingplatzi/util/StringUtil.java
@@ -12,13 +12,23 @@ public class StringUtil {
         if (times <0){
             throw new IllegalArgumentException("negative times not allowed");
         }
-        //StringBuilder result = new StringBuilder();  /*Otra forma de Hacer funcionar con mejor codigo*/
-        String result = "";
+        StringBuilder result = new StringBuilder();  /*Otra forma de Hacer funcionar con mejor codigo*/
+        //String result = "";
         for (int i = 0; i < times; i++) {
-            result += str;
-            //result.append(str);  /*Usado con el StringBuilder*/
+            //result += str;
+            result.append(str);  /*Usado con el StringBuilder*/
         }
-        //return result.toString();/*Usado con el StringBuilder*/
-        return result;
+        return result.toString();/*Usado con el StringBuilder*/
+        //return result;
+    }
+
+    /**
+     * Metodo para revisar si un String esta vacio
+     * @param str el string a comprobar si es vacio
+     * @return true si es vacio, false si no lo es
+     */
+    public static boolean isEmpty(String str){
+
+        return str == null || str.trim().length() == 0;
     }
 }
--- src/test/java/com/platzi/javatestingplatzi/util/StringUtilTest.java
+++ src/test/java/com/platzi/javatestingplatzi/util/StringUtilTest.java
@@ -2,8 +2,10 @@ package com.platzi.javatestingplatzi.util;
 
 
 
-import org.junit.Assert;
+import org.junit.Assert.*;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 /**
  * Clase para testeado de lo ya contruido, tenemos un main para la prueba del metodo aqui en la clase de test
@@ -13,24 +15,41 @@ public class StringUtilTest {
     @Test
     public  void repeat_string_once() {
         //Usando la funcion assertEquals de Junit
-        Assert.assertEquals("hola", StringUtil.repeat("hola",1));
+        assertEquals("hola", StringUtil.repeat("hola",1));
     }
     @Test
     public  void repeat_string_multiple_times() {
         //Llamado a la funcion a probar
         //Usando la funcion assertEquals de Junit
-        Assert.assertEquals("holaholahola",StringUtil.repeat("hola",3));
+        assertEquals("holaholahola",StringUtil.repeat("hola",3));
     }
     //Comprobacion si se repite 0 veces
     @Test
     public void repeat_string_zero_times(){
-        Assert.assertEquals("",StringUtil.repeat("hola",0));
+        assertEquals("",StringUtil.repeat("hola",0));
     }
     //Test para repetir negativo, es algo inesperado y con lo que deberiamos retornar es una excepcion
     @Test(expected = IllegalArgumentException.class)
     public void repeat_string_negative_times(){
        StringUtil.repeat("hola",-1);
     }
+    //Test de prueba función String vacío
+    @Test
+    public void string_is_not_empty() {
+        assertFalse(StringUtil.isEmpty("hola"));
+    }
 
+    @Test
+    public void string_is_empty() {
+        assertTrue(StringUtil.isEmpty(""));
+    }
 
+    @Test
+    public void string_null_is_empty() {
+        assertTrue(StringUtil.isEmpty(null));
+    }
+    @Test
+    public void string_with_spaces_is_empty() {
+        assertTrue(StringUtil.isEmpty("  "));
+    }
 }
