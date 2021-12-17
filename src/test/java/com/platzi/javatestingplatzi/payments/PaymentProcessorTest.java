d1a5b8e219d4c3cec82a2782862039531dc19162
elkinfernandez21 <elkinfernandez21@gmail.com>
2021-12-17 13:39:20 -0500

    Uso de etiquetas @Mock y @InjectMocks para reducir aún más el código de inyeccion de un mock sobre una instancia de una clase

--- src/test/java/com/platzi/javatestingplatzi/payments/PaymentProcessorTest.java
+++ src/test/java/com/platzi/javatestingplatzi/payments/PaymentProcessorTest.java
@@ -1,8 +1,12 @@
 package com.platzi.javatestingplatzi.payments;
 
-import org.junit.Before;
+//import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
 
@@ -10,20 +14,28 @@ import static org.junit.Assert.*;
  * En esta clase podemos definir campos privados o variables en la clase de test
  * que usaremos como campos de la clase test
  */
-public class PaymentProcessorTest {
 
+@RunWith(MockitoJUnitRunner.class)
+public class PaymentProcessorTest {
+    //Crea un Mock
+    @Mock
     private PaymentGateway paymentGateway;
+    //Crea una instancia de la clase y inyecta el mock que fue creado con la anotacion @Mock dentro de esta instancia
+    @InjectMocks
     private PaymentProcessor paymentProcessor;
 
     /**
-     * Ejecutara este metodo antes de cada metodo, esto regenera el escenario para cada test
+     * Ejecutara este metodo antes de cada metodo, esto regenera el escenario para cada test, no se necesitara
+     * si se usa el @Mock y el @InjectMocks, si no se usa el @RunWith, se puede usar el  Mockito.initMocks(this)
+     * en este metodo Before, esto asegura la creacion del Mock y su respectiva inyeccion
      */
-    @Before
+    /*@Before
     public void setup(){
+         Mockito.initMocks(this)
         //Al tener una interfaz no podemos instanciar la clase por eso, usando Mockito podemos crearnos una simulacion de esa implementacion para probarla en el PaymentProcessor
         paymentGateway = Mockito.mock(PaymentGateway.class);
         paymentProcessor = new PaymentProcessor(paymentGateway);
-    }
+    }*/
 
 
     @Test
