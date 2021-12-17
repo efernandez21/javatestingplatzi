d7ca2af6c0ab8367170c37225bf27855e27bf371
elkinfernandez21 <elkinfernandez21@gmail.com>
2021-12-17 13:23:52 -0500

    Refactorizacion de los Test y agregacion de @Before para la preparacion de objetos

--- src/test/java/com/platzi/javatestingplatzi/payments/PaymentProcessorTest.java
+++ src/test/java/com/platzi/javatestingplatzi/payments/PaymentProcessorTest.java
@@ -1,31 +1,42 @@
 package com.platzi.javatestingplatzi.payments;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
 
+/**
+ * En esta clase podemos definir campos privados o variables en la clase de test
+ * que usaremos como campos de la clase test
+ */
 public class PaymentProcessorTest {
+
+    private PaymentGateway paymentGateway;
+    private PaymentProcessor paymentProcessor;
+
+    /**
+     * Ejecutara este metodo antes de cada metodo, esto regenera el escenario para cada test
+     */
+    @Before
+    public void setup(){
+        //Al tener una interfaz no podemos instanciar la clase por eso, usando Mockito podemos crearnos una simulacion de esa implementacion para probarla en el PaymentProcessor
+        paymentGateway = Mockito.mock(PaymentGateway.class);
+        paymentProcessor = new PaymentProcessor(paymentGateway);
+    }
+
 
     @Test
     public void payment_is_correct() {
-        //Al tener una interfaz no podemos instanciar la clase por eso, usando Mockito podemos crearnos una simulacion de esa implementacion para probarla en el PaymentProcessor
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         //El moquito any se usa para simular una parametro o solicitud cualquiera que necesite un metodo para funcionar, hay veces que se necesita castear el parametro que se vuelve el Mockito.any()
         Mockito.when(paymentGateway.requestPayment(Mockito.any())).thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
-
         //Comprobacion del funcionamiento del pago
         assertTrue(paymentProcessor.makePayment(1000));
     }
     @Test
     public void payment_is_incorrect() {
-        //Al tener una interfaz no podemos instanciar la clase por eso, usando Mockito podemos crearnos una simulacion de esa implementacion para probarla en el PaymentProcessor
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         //El moquito any
         Mockito.when(paymentGateway.requestPayment(Mockito.any())).thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
-
         //Comprobacion del funcionamiento del pago
         assertFalse(paymentProcessor.makePayment(1000));
     }
